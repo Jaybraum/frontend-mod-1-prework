0f6ab8f94c6fe994d5eed8e18bbc9a2293914c4d
Jermaine Braumuller <braumuller02@gmail.com>
2021-01-16 02:54:04 +0100

    Add return to exit function

--- finalProject/tenSpeckledFrogs.js
+++ finalProject/tenSpeckledFrogs.js
@@ -4,6 +4,6 @@ for (var i = 10; i < frog.length; i--) {
   if (i > 0) {
     console.log(+[i] + " speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there was " + [i - 1] + " speckled frog.");
   } else if (i === 0) {
-    console.log(+[i] + " speckled frog sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were no more speckled frogs!");
+    console.log(+[i] + " speckled frog sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were no more speckled frogs!");return;
   }
 }console.log(frog[i]);
